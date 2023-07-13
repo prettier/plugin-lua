a283f47bc3fae7a497ac42708173d6ecaabde880
Lily Skye <git@suchipi.tv>
2023-07-12 19:25:11 -0600

    add test for prettier-ignore comment

---
+++ tests/comments/prettier-ignore.lua
@@ -0,0 +1,32 @@
+weird_table_1 = {
+    1, 2
+    , { a = 5 }
+      }
+
+-- prettier-ignore
+weird_table_2 = {
+        1,       2
+  , { a = 5          }
+    } 
+
+-- prettier-ignore for reasons
+weird_table_3 = {
+  1,       2
+, { a = 5          }
+} 
+
+--[==[ prettier-ignore ]==]
+weird_table_4 = {
+  1, 2
+, { a =
+
+5 }
+}
+
+--[==[
+  prettier-ignore for reasons
+]==]
+weird_table_5 = {
+                              1, 2
+  , { a = 5 }
+    }
